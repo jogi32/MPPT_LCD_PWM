ced2902d12f1d8caaf9705966ba92cba91d18761
jogi32 <tomasz.szafranski32@gmail.com>
2015-08-13 22:06:24 +0200

    MPPT
    Skeleton of code ported from local project

---
+++ MPPT_LCD_PWM.c
@@ -0,0 +1,18 @@
+/*
+ * MPPT_LCD_PWM.c
+ *
+ * Created: 2015-08-13 21:57:02
+ *  Author: TomaszSzafrański
+ */ 
+
+
+#include <avr/io.h>
+
+int main(void)
+{
+    while(1)
+    {
+		
+        //TODO:: Please write your application code 
+    }
+}
